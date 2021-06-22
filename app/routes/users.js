85c8427637376ce4c6a912bc777a92beebd53488
Tom <cocplayer1alpha@gmail.com>
2021-06-23 02:10:58 +0700

    include the functions for the routes of /users in the server

---
+++ app/routes/users.js
@@ -0,0 +1,73 @@
+const User = require('../models/users.js');
+
+/**
+ * GET /users retrieve all the users
+ */
+function getUsers(req, res, next) {
+    let query = User.find({});
+    query.exec((err, users) => {
+        if(err || !users)  return res.status(404).send(err);
+        res.status(200).json(users);
+    });
+}
+
+/**
+ * POST /users create a new user
+ */
+function createNewUser(req, res, next) {
+    const newUser = new User(req.body);
+    newUser.save((err, user) => {
+        if(err)  return res.status(400).send();
+        res.status(201).json(user);
+    });
+}
+
+/**
+ * GET /users/:id retrieve an user given its id
+ */
+function getUser(req, res, next) {
+    User.findOne({userId: req.params.id}, (err, user) => {
+        if(err || !user)  return res.status(404).send();
+        res.status(200).json(user);
+    });
+}
+
+/**
+ * DELETE /users/:id delete an user given its id
+ */
+function deleteUser(req, res, next) {
+    User.deleteOne({userId: req.params.id}, (err, result) => {
+        if(err)  return res.status(404).send();
+        
+        //TODO: delete the record of the user in projects and tasks databases
+
+        res.status(204).json(result);
+    });
+}
+
+/**
+ * PUT /users/:id update 
+ */
+function updateUser(req, res, next) {
+    if(req.body.userId || req.body.projectIds || req.body.taskIds)  return res.status(400).send();
+    User.findOne({userId: req.params.id}, (err, user) => {
+        if(err || !user)  return res.status(404).send();
+        Object.assign(user, req.body).save((err, user) => {
+            if(err)  return res.status(500).send();
+            res.status(200).json(user);
+        });
+    });
+}
+
+/**
+ * GET /users/:id/projects retreieve all projects an user is working on
+ */
+function getProjects(req, res, next) {
+    User.findOne({userId: req.params.id}, (err, user) => {
+        if(err || !user)  return res.status(404).send();
+        res.status(200).json(user.projectIds);
+    });
+}
+
+
+module.exports = { getUsers, createNewUser, getUser, deleteUser, updateUser, getProjects };
