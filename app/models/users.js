7f915844c4598f6960e99c8dbfa0e4095f2f16af
Tom <cocplayer1alpha@gmail.com>
2021-06-23 02:09:56 +0700

    include the schema for users in database

---
+++ app/models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        userId: {type: String, unique: true, required: true, lowercase: true, trim: true},
+        name: {type: String, required: true},
+        DOB: {type: Date, default: Date.now},
+        pass: {type: String, required: true}, //TODO update later
+        projectIds: [Schema.Types.ObjectId],
+        taskIds: [Schema.Types.ObjectId]
+    },
+    {
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model('user', UserSchema);
