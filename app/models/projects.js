5acfc24406b87cadd12a9918a450eebc95109312
Tom <cocplayer1alpha@gmail.com>
2021-06-24 22:36:15 +0700

    include the schema for projects in database

---
+++ app/models/projects.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        content: {type: String, required: true},
+        dayStart: {type: Date, default: Date.now},
+        deadline: {type: Date},
+        completed: {type: Boolean, default: false},
+        budget: {type: Number, min: 0, default: 0},
+        users: [{
+            userId: {type: String, required: true, lowercase: true, trim: true},
+            roles: [String],
+            taskIds:[String]
+        }],
+        taskIds:[String]
+    },
+    {
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model('project', ProjectSchema);
