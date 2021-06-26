5e3e431aaf616a431ea78a97824ce0e1668c3eaf
Tom <cocplayer1alpha@gmail.com>
2021-06-27 00:58:31 +0700

    add functions for /projects routes

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -5,11 +5,21 @@ const User = require('../models/users.js');
  * GET /projects retrieve all the projects
  */
 function getProjects(req, res, next) {
-    let query = Project.find({});
-    query.exec((err, projects) => {
-        if(err || !projects)  return res.status(404).send(err);
-        res.status(200).json(projects);
-    });
+    getManyProjects(res);
+}
+
+/**
+ * GET /projects/incomplete retrieve all completed projects
+ */
+ function getIncompleteProjects(req, res, next) {
+    getManyProjects(res, false);
+}
+
+/**
+ * GET /projects/completed retrieve all completed projects
+ */
+function getCompletedProjects(req, res, next) {
+    getManyProjects(res, false, true);
 }
 
 /**
@@ -103,9 +113,7 @@ function includeUser(req, res, next) {
             //TODO: update tasks database
 
             const userIndex = project.users.findIndex(user => user.userId === userId);
-            console.log(userIndex);
             if(userIndex === -1) {
-                console.log('ec');
                 const newProjectUser = {
                     userId: userId,
                     roles: req.body.roles,
@@ -118,7 +126,6 @@ function includeUser(req, res, next) {
                 });
             }
             else {
-                console.log('ec ec');
                 projectUser = project.users[userIndex];
                 projectUser.roles = projectUser.roles.concat(req.body.roles);
                 projectUser.taskIds = projectUser.taskIds.concat(req.body.taskIds);
@@ -131,23 +138,77 @@ function includeUser(req, res, next) {
     });
 }
 
-module.exports = { getProjects, createNewProject, getProject, deleteProject, updateProject, getUsers, includeUser };
+/**
+ * GET /projects/:id/users/:index retrieve the user users[index] of the project
+ */
+function getUser(req, res, next) {
+    const index = parseInt(req.params.index);
+    Project.findById(req.params.id, (err, project) => {
+        if(err || !project)  return res.status(404).send();
+        if(index < 0 || index >= project.users.length)  return res.status(404).send();
+        res.status(200).json(project.users[index]);
+    });
+}
+
+/**
+ * DELETE /projects/:id/users/:index exclude the user users[index] from the project
+ */
+function deleteUser(req, res, next) {
+    const index = parseInt(req.params.index);
+    Project.findById(req.params.id, (err, project) => {
+        if(err || !project)  return res.status(404).send();
+        if(index < 0 || index >= project.users.length)  return res.status(404).send();
+        
+        updateUserDB(project.users[index].userId, project.id, project.users[index].taskIds, true);
+
+        project.users.splice(index, 1);
+        project.save((err, project) => {
+            if(err)  return res.status(500).send();
+            res.status(204).send();
+        });
+    });
+}
+
+module.exports = { getProjects, getCompletedProjects, getIncompleteProjects, createNewProject, getProject, deleteProject, updateProject, getUsers, includeUser,
+                    getUser, deleteUser, /*updateUser*/ };
 
 //helper functions
+/**
+ * Get the projects for many projects at once
+ * @param {*}       res         the response of the route
+ * @param {boolean} all         whether the response contain all projects
+ * @param {boolean} completed   whether the response contain completed or incomplete projects
+ */
+function getManyProjects(res, all=true, completed=false) {
+    let query;
+    if(all)  query = Project.find({});
+    else  query = Project.find({completed: completed});
+
+    query.exec((err, projects) => {
+        if(err || !projects)  return res.status(404).send(err);
+        res.status(200).json(projects);
+    });
+}
+
+/**
+ * Update to the User database
+ * @param {string}          userId      the id of the updating user
+ * @param {ProjectSchema}   project     the project for update
+ * @param {[string]}        taskIds     the array of taskId for update
+ * @param {boolean}         toDelete    whether the update is deletion or insertion
+ */
 function updateUserDB(userId, project, taskIds=[], toDelete=false) {
     User.findById(userId, (err, user) => {
-        if(!err && user) {
-            if(toDelete) {
-                user.projectIds = user.projectIds.filter(id => id !== project.id);
-                user.save();
-            }
-            else {
-                user.projectIds.push('' + project.id);
-                // user.taskIds = user.taskIds.concat(taskIds);
-                user.save();
-            }
+        if(!err && user && toDelete) {
+            user.projectIds = user.projectIds.filter(id => id !== project.id);
+            user.save();
         }
-        else {
+        else if (!err && user && !toDelete) {
+            user.projectIds.push('' + project.id);
+            // user.taskIds = user.taskIds.concat(taskIds);
+            user.save();
+        }
+        else if(!toDelete) {
             const index = project.users.findIndex(projectUser => projectUser.userId === userId);
             project.users.splice(index, 1);
         }
